3303040f14697aa1e1555f18ccbf882c6298974c
Vinicius Henrique Gonçalves Matos <viniciushenrique.gm@hotmail.com>
2025-11-02 15:03:15 -0300

    projeto

---
+++ MauiAppHotel/Views/sobre.xaml.cs
@@ -0,0 +1,10 @@
+namespace MauiAppHotel.Views
+{
+    public partial class SobrePage : ContentPage
+    {
+        public SobrePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
