73873ed894d0e8b2977db0bbade79441195fb18e
Vinicius Henrique Gonçalves Matos <viniciushenrique.gm@hotmail.com>
2025-11-09 17:05:02 -0300

    add novos recursos

--- MauiAppHotel/AppShell.xaml.cs
+++ MauiAppHotel/AppShell.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace MauiAppHotel
+﻿using MauiAppHotel.Views;
+
+namespace MauiAppHotel
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+;
         }
     }
 }
